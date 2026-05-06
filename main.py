bacfbf293870b75003c0813eead45a3cf69e620d
nos-es <ethemsahbaz@outlook.com>
2026-05-06 09:26:54 +0200

    added constants file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pygame
+import constants
+
+
 def main():
-    print("Hello from asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
