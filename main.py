e0f531a1903a8d3e9cc3d0d2ccb18306f12dbb68
nos-es <ethemsahbaz@outlook.com>
2026-05-08 07:13:38 +0200

    moved player to groups

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,8 +22,10 @@ def main():
                 return
         dt = clock.tick(60)/1000
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for el in drawable:
+            el.draw(screen)
+
         pygame.display.flip()
 
         print(dt)
